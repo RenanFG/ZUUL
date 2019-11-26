5265ded043e9961bf7c8912d61902e4e8f0d1a97
rafaelarriel <58053515+rafaelarriel@users.noreply.github.com>
2019-11-26 14:19:39 -0300

    Add files via upload

--- MODworldofzuul/Janela.java
+++ MODworldofzuul/Janela.java
@@ -1,34 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package rpgcorp.zuul;
-
-/**
- *
- * @author renan
- */
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.FlowLayout;
 import javax.swing.*;
+
 public class Janela {
-private JFrame janela_principal;
-private JLabel nTentivaRestantes;
-private JLabel nTentativas;
-private JLabel durChave;
-private JLabel nDurChave;
-private JLabel dEncontradas_label; 
-private JLabel dEncotradas;
+
+	private JFrame janela_principal;
+
+	private JLabel nTentivaRestantes;
+	private JLabel nTentativas;
+
+	private JLabel durChave;
+	private JLabel nDurChave;
+
+	private JLabel dEncontradas_label; 
+	private JLabel dEncotradas;
+	
+	private JLabel bemVimdo;
+	private JLabel localAtual;
+	private JTextField comando; 
+	
+	private JLabel image;
+
 
     public Janela() {
+        
         janela_principal = new JFrame("World of ZUUL");
         nTentivaRestantes = new JLabel("Numero de Tentaivas Restantantes");
         nTentativas = new JLabel("n");
+        
         durChave = new JLabel("Durabilidade da cheve mestra");
         nDurChave = new JLabel("N");
+        
         dEncontradas_label = new JLabel("Dicas Encontradas");
         dEncotradas = new JLabel("N");
+        
+        bemVimdo = new JLabel("Bem Vindo ao Jogo World of ZUUL");
+        localAtual = new JLabel("Voce esta na cozinha");
+        comando = new JTextField();
+
+        image = new JLabel(new ImageIcon("1f4d140f9b1a7881f0d8a9d07b04c952a3099151r1-480-480v2_hq.jpg"));
         
         montarJanela();
     }
@@ -37,7 +48,8 @@ private JLabel dEncotradas;
         
         janela_principal.setSize(1280,720);
         janela_principal.setLayout(new BorderLayout());
-                
+            
+        //painel esquerda        
         JPanel painelEsq = new JPanel();
         painelEsq.setLayout(new GridLayout(4,1));
         painelEsq.add(nTentivaRestantes);
@@ -45,8 +57,29 @@ private JLabel dEncotradas;
         painelEsq.add(durChave);
         painelEsq.add(nDurChave);
         janela_principal.add(painelEsq,BorderLayout.WEST);
-
-        janela_principal.pack();
+		
+		//painel direita
+		JPanel painelDir = new JPanel();
+        painelDir.setLayout(new GridLayout(2,1));
+        painelDir.add(dEncontradas_label);
+        painelDir.add( dEncotradas);
+        janela_principal.add(painelDir,BorderLayout.EAST);
+        
+        //painel sul/abaixo
+        JPanel painelSul = new JPanel();
+        painelSul.setLayout(new GridLayout(3,1));
+        painelSul.add(bemVimdo);
+        painelSul.add(localAtual);
+        painelSul.add(comando);
+        janela_principal.add(painelSul,BorderLayout.SOUTH);
+        
+        //painelcentral
+        JPanel painelCent = new JPanel();
+        painelCent.setLayout(new FlowLayout());
+        painelCent.add(image);
+        janela_principal.add(painelCent,BorderLayout.CENTER);
+        
+       janela_principal.pack();
         
     }
     public void exibir(){
